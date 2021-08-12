364289329e67590854cde13c53e0d5149adbb840
Jacko308 <73738729+Jacko308@users.noreply.github.com>
2021-08-12 18:11:13 +1200

    updates

--- Assignment2.py
+++ Assignment2.py
@@ -1,52 +1,64 @@
 import cv2 as cv
 import numpy as np
-#import matplotlib.pyplot as plt
+
 
 def main():
 
-    # load input images for demonstration
-    input_rice = cv.imread("input/cocopops.jpg", cv.IMREAD_GRAYSCALE)
+    # ---------------- Load image
 
-    # local adaptive thresholding - computes local threshold based on given window size
-    output_adapthresh = cv.adaptiveThreshold (input_rice, 255.0,
-            cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 51, -20.0)
-    cv.imshow("Adaptive Thresholding", output_adapthresh)
-    #cv.imwrite('rice_adapthresh.png', output_adapthresh)
+    img = cv.imread("data/cocopops.jpg")
+    if img is None:
+        print("ERROR::CV::Could not read image.")
+        return 1
+
+    # Resize image
+    rows, cols, channels = img.shape
+    rows = rows // 4
+    cols = cols // 4
+
+    img = cv.resize(img, (cols, rows))
+
+    cv.imshow("cocopops", img)
+    cv.imwrite('../images/01/01.PNG', img)
+
+    # Convert image from BGR to HSV
+    blurred = cv.GaussianBlur(img, (7, 7), 0)
+    hsv = cv.cvtColor(blurred,cv.COLOR_BGR2HSV)
+
+#-------------- Create masks from HSV values
+    lower_1 = np.array([0,99,0])
+    upper_1 = np.array([179,255,255])
+    mask1 = cv.inRange(hsv, lower_1, upper_1)
+    # mask1 = ~mask1
+    cv.imshow('mask1', mask1)
+    kernel = np.ones((2,2),np.uint8)
+
+    #--------------- Morphological Transforms ----------------------#
+    closing = cv.morphologyEx(mask1, cv.MORPH_CLOSE, kernel)
+    # erodeCocopops = cv.erode(mask1,kernel,iterations = 4)
+    # cv.imshow('erodeCocopops', erodeCocopops)
+    # dilateCocopops = cv.dilate(erodeCocopops, kernel, iterations = 3)
+    # cv.imshow('dilateCocopops', dilateCocopops)
+
+    #-------------- Thresholding -----------#
+    # ret1, thresh = cv.threshold(cocopopsleft, 240, 255, 1)
+    num = 0
+    contours,h  = cv.findContours(closing,1,2)
+    for cnt in contours:
+            area = cv.contourArea(cnt)
+            if ((area >= 270) & (area <= 1000)):
+                print("contour num = {}\ncontour area = {}".format(cnt,area))
+                cv.drawContours(img,[cnt],0,(0,0,255),2)
+                num += 1
+                # cv.imshow("contour num = {}".format(num), img)
+                # cv.waitKey(0)
+    cv.putText(img, "Number of single Cocopops = " + "{}".format(num), (30, 700), cv.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 0), 2)            
+    cv.imshow('img001', img)
+
     cv.waitKey(0)
-    # morphologial erosion - cleaning up binary images
-    kernel = np.ones((5,5),np.uint8)
-    output_erosion = cv.erode(output_adapthresh, kernel)
-    cv.imshow("Morphological Erosion", output_erosion)
-    #cv.imwrite('rice_erosion.png', output_erosion)
+    cv.destroyAllWindows()   
 
-    # connected components - counts and marks number of distinct foreground objects
-    # apply connected components on clean binary image
-    # label_image = output_erosion.copy()
-    # label_count = 0
-    # rows, cols = label_image.shape
-    # for j in range(rows):
-    #     for i in range(cols):
-    #         pixel = label_image[j, i]
-    #         if 255 == pixel:
-    #             label_count += 1
-    #             cv.floodFill(label_image, None, (i, j), label_count)
-
-    # print("Number of foreground objects", label_count)
-    # cv.imshow("Connected Components", label_image)
-    # #cv.imwrite('rice_components.png', label_image)
-
-    # # Contours - Computes polygonal contour boundary of foreground objects
-    # # apply connected components on clean binary image
-    # _, contours, _ = cv.findContours(output_erosion, cv.RETR_EXTERNAL,  cv.CHAIN_APPROX_SIMPLE)
-    # output_contour = cv.cvtColor(input_rice, cv.COLOR_GRAY2BGR)
-    # cv.drawContours(output_contour, contours, -1, (0, 0, 255), 2)
-    # print("Number of detected contours", len(contours))
-    # cv.imshow("Contours", output_contour)
-    #cv.imwrite('rice_contours.png', output_contour)
-
-    # wait for key press
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    return 0
 
 
 if __name__ == "__main__":
--- Threshold.py
+++ Threshold.py
@@ -1,76 +1,45 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 def main():
 
-    # Load image
+    # ---------------- Load image ----------------- #
 
-    img = cv.imread("data/cocopops.jpg", 0)
-
-    if img is None:
-        print("ERROR::CV::Could not read image.")
-        return 1
+    img = cv.imread("data/Wire_Straight.jpg")
 
     # Resize image
-
-    rows, cols = img.shape
-    
-    rows = rows // 2
-    cols = cols // 2
+    rows, cols, channels = img.shape
+    rows = rows // 4
+    cols = cols // 4
 
     img = cv.resize(img, (cols, rows))
 
-    cv.imshow("cocopops", img)
-    cv.imwrite('../images/01/01.PNG', img)
+    cv.imshow("Straight Wire", img)
+   
+    # ---- Convert image from BGR to grayscale
 
-    # Convert image from BGR to grayscale
+    gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    kernel = np.ones((3,3),np.uint8)
 
-    # gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    #--------------- Morphological Transforms ----------------------#
-    kernel = np.ones((5,5),np.uint8)
-    erodeCocopops = cv.erode(img,kernel,iterations = 5)
-    dilateCocopops = cv.dilate(erodeCocopops, kernel, iterations = 5)
-    blurred = cv.GaussianBlur(img, (7, 7), 0)
-
-    background = dilateCocopops
-
-    #-------------- Operations -------------#
-    cocopopsleft = img - background
-    cv.imshow('cocopopsleft', cocopopsleft)
-    cv.waitKey(0)
     #-------------- Thresholding -----------#
-    ret1, th1 = cv.threshold(cocopopsleft,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-    #thresh = cv.adaptiveThreshold(blurred, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 21, 6)
-    # cv.imshow("Thresh_cocopops", ret1)
-    #cv.imshow("gaussian Adaptive Thresholding", thresh)
+    ret1, thresh = cv.threshold(gray_img, 80, 255, cv.THRESH_BINARY_INV)
+    cv.imshow('Binary Threshold', thresh)
 
-    ret1, contours = cv.findContours(ret1, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-    maxS = -1
+    num = 0
+    contours,h  = cv.findContours(thresh,1,2)
     for cnt in contours:
-            tempS = cv.contourArea(cnt)
-            if maxS < tempS:
-                maxS = tempS
-                maxC = tempC = cv.arcLength(cnt,True)
-                contour = cnt
-    
-    cv.drawContours(img, [contour], -1, (0,0,255,),1)
-    cv.imshow('cocopopsleft2', cocopopsleft)
-    cv.imshow('img', img)
-    print('Maximum area: ', maxS)
-    print('Corresponding to the perimeter of the rice grain: ', maxC)
-
-
-
+            area = cv.contourArea(cnt)
+            # if ((area >= 270) & (area <= 1000)):
+            print("contour num = {}\ncontour area = {}".format(cnt,area))
+            cv.drawContours(img,[cnt],0,(0,0,255),2)
+            num += 1
+            cv.imshow("contour num = {}".format(num), img)
+            cv.waitKey(0)
+    # cv.putText(img, "Number of single Cocopops = " + "{}".format(num), (30, 700), cv.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 0), 2)            
+    cv.imshow('img001', img)
     cv.waitKey(0)
-
-    # Generate histogram
-    # plt.hist(img.ravel(), 256, [0, 256])
-    # plt.savefig("data/threshold_histogram.png")
-    # plt.savefig("02.PNG")
-    # plt.show()
-    cv.waitKey(1)
     cv.destroyAllWindows()   
 
     return 0
