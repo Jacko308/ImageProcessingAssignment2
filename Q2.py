801ac526a07d6935066fc9e84d8e282e2eb344d3
Jacko308 <73738729+Jacko308@users.noreply.github.com>
2021-08-15 15:23:49 +1200

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -5,7 +5,7 @@ import numpy as np
 def main():
 
     # ---------------- Load image
-    img = cv.imread("images/Q2/mandarine.jpg")
+    img = cv.imread("images/Q2/mandarin_calsq.jpg")
     if img is None:
         print("ERROR::CV::Could not read image.")
         return 1
@@ -19,17 +19,26 @@ def main():
 
     cv.imshow("mandarine", img)
     # Convert image from BGR to HSV
-    blurred = cv.GaussianBlur(img, (7, 7), 0)
+    blurred = cv.GaussianBlur(img, (3, 3), 0)
     hsv = cv.cvtColor(blurred,cv.COLOR_BGR2HSV)
 
 #-------------- Create masks from HSV values
-    lower_1 = np.array([0,99,0])
+    cal_lower_1 = np.array([76,151,0])
+    cal_upper_1 = np.array([93,0,119])
+    cal_mask1 = cv.inRange(hsv, cal_lower_1, cal_upper_1)
+    # mask1 = ~mask1
+    cv.imshow('mask1', cal_mask1)
+
+
+    lower_1 = np.array([0,106,0])
     upper_1 = np.array([179,255,255])
     mask1 = cv.inRange(hsv, lower_1, upper_1)
     # mask1 = ~mask1
     cv.imshow('mask1', mask1)
     kernel = np.ones((2,2),np.uint8)
 
+
+    
     #--------------- Morphological Transforms ----------------------#
     closing = cv.morphologyEx(mask1, cv.MORPH_CLOSE, kernel)
     # erodeCocopops = cv.erode(mask1,kernel,iterations = 4)
@@ -43,13 +52,13 @@ def main():
     contours,h  = cv.findContours(closing,1,2)
     for cnt in contours:
             area = cv.contourArea(cnt)
-            # if ((area >= 270) & (area <= 1000)):
+            # if ((area >= 270) & (area <= 1000)): 
             print("contour num = {}\ncontour area = {}".format(cnt,area))
             cv.drawContours(img,[cnt],0,(0,0,255),2)
             num += 1
             cv.imshow("contour num = {}".format(num), img)
             cv.waitKey(0)
-    cv.putText(img, "surface area of mandarine = " + "{}".format(area), (30, 900), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 0), 2)            
+    cv.putText(img, "surface area of mandarine = " + "{}".format(area), (30, 700), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 0), 2)            
     cv.imshow('Q2_result', img)
     # cv.imwrite('images/Q2/Q2_result.png', img)
     cv.waitKey(0)
